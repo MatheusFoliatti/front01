7d78cb01303f54e014f0578b9faa34f7144a562b
Matheus Folitti <matheusfoliatti@gmail.com>
2023-05-12 20:27:25 -0300

    styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
 import "./App.css";
 
-function App() {
-	const [count, setCount] = useState(0);
-
+export const App = () => {
+	const txt = "ZAWARUDO";
 	return (
 		<>
 			<p>Hello World</p>
+			<p>{txt}</p>
 		</>
 	);
-}
-
-export default App;
+};
