06176185fcf2920869794aba11ae89823f7c57d3
Matheus Folitti <matheusfoliatti@gmail.com>
2023-05-12 21:55:03 -0300

    button proprietitas

--- src/App.style.js
+++ src/App.style.js
@@ -12,4 +12,27 @@ export const Box = styled.div`
 		font-weight: bold;
 		color: blueviolet;
 	}
+
+	button{
+		padding: 15px;
+		border: 0;
+		border-radius: 8px;
+		background: #3f3f;
+		outline: 1px solid black;
+		color: red;
+
+		&:hover{
+			background: white;
+			color: black;
+
+		&:focus{
+			border: 3px solid blue;
+		}
+
+		&:active{
+			background:cyan;
+			box-shadow: 3px 5px 8px 3px #000;
+		}
+	}
+}
 `;
