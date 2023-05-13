0884110387db6d7b58819dbe8e53a47d7e32364a
Matheus Folitti <matheusfoliatti@gmail.com>
2023-05-12 22:26:40 -0300

    nomezinhos

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
 import "./App.css";
+import axios from "axios";
 
 import { Box } from "./App.style";
+import { useState } from "react";
 
 export const App = () => {
+	const [person, setPerson] = useState({
+		name: {
+			first: "???",
+		},
+	});
+
+	//Váriavel não reativa. Não irá renderizar
+	//const PersonNReact = {};
+
+	async function handleGet() {
+		const data = await axios.get("https://randomuser.me/api/?nat=br");
+
+		setPerson(data.data.results[0]);
+		// console.log(data.data.results[0].name.first);
+
+		console.log(person);
+	}
+
 	const txt = "ZAWARUDO";
 	return (
 		<>
 			<Box>
-				<p>Hello World</p>
-				<p>{txt}</p>
-				<button>Request </button>
+				<p>Nome: {person.name.first} </p>
+
+				<button onClick={handleGet}>Request </button>
 			</Box>
 		</>
 	);
