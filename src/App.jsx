82385f320ac1f428a4233a89cff1942b10777da5
Matheus Folitti <matheusfoliatti@gmail.com>
2023-05-12 21:40:40 -0300

    componentizando styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
-import styled from "styled-components";
 import "./App.css";
 
-const PStyle = styled.p`
-	font-size: 20px;
-	color: purple;
-`;
-
-const Box = styled.div`
-	width: 400px;
-	height: 400px;
-	background-color: aliceblue;
-	border: 2px solid red;
-	border-radius: 10px;
-
-	p {
-		font-size: 50px;
-		font-weight: bold;
-		color: blueviolet;
-	}
-`;
+import { Box } from "./App.style";
 
 export const App = () => {
 	const txt = "ZAWARUDO";
 	return (
 		<>
 			<Box>
-				<PStyle>Hello World</PStyle>
+				<p>Hello World</p>
 				<p>{txt}</p>
 			</Box>
 		</>
---
+++ src/App.style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Box = styled.div`
+	width: 400px;
+	height: 400px;
+	background-color: aliceblue;
+	border: 2px solid red;
+	border-radius: 10px;
+
+	p {
+		font-size: 50px;
+		font-weight: bold;
+		color: blueviolet;
+	}
+`;
